7cf5ec4c23a38a74742290b55b08a22a3c9bcfda
Alwaleed Ahmed <alwleedamado@gmail.com>
2023-10-18 19:21:49 +0200

    intial commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import User from './user'
+import Pin from './pin'
+import postedby from './postedby'
+import save from './save'
+import comment from './comment'
+
+export const schemaTypes = [User, Pin, postedby, save, comment]
